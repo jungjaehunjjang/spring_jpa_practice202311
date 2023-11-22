49bb22b9c121f4b6d8395fe4e00cf050ab4d9362
jaehun <josh9269@naver.com>
2023-11-22 15:11:31 +0900

    #9. 종합 연습예제 3(posts -> POST)

--- src/main/java/com/study/jpa/chap05_practice/service/PostService.java
+++ src/main/java/com/study/jpa/chap05_practice/service/PostService.java
@@ -1,9 +1,7 @@
 package com.study.jpa.chap05_practice.service;
 
-import com.study.jpa.chap05_practice.dto.PageDTO;
-import com.study.jpa.chap05_practice.dto.PageResponseDTO;
-import com.study.jpa.chap05_practice.dto.PostDetailResponseDTO;
-import com.study.jpa.chap05_practice.dto.PostListResponseDTO;
+import com.study.jpa.chap05_practice.dto.*;
+import com.study.jpa.chap05_practice.entity.HashTag;
 import com.study.jpa.chap05_practice.entity.Post;
 import com.study.jpa.chap05_practice.repository.HashTagRepository;
 import com.study.jpa.chap05_practice.repository.PostRepository;
@@ -16,6 +14,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.validation.Path;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -49,14 +48,55 @@ public class PostService {
                 .map(PostDetailResponseDTO::new)
                 .collect(Collectors.toList());
 
-
         // DB에서 조회한 정보를 JSON 형태에 맞는 DTO로 변환 -> PostListResponseDTO
         return PostListResponseDTO.builder()
                 .count(detailList.size()) // 총 게시물 수가 아니라 조회된 게시물의 개수
                 .pageInfo(new PageResponseDTO(posts)) // 페이지 정보가 담긴 객체를 DTO에게 전달해서 그쪽에서 처리하게 함.
                 .posts(detailList)
                 .build();
+    }
+
+    public PostDetailResponseDTO getDetail(Long id) throws Exception {
+
+        Post postEntity = postRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException(id + "번 게시물이 존재하지 않습니다!"));
+
+        return new PostDetailResponseDTO(postEntity);
+        
+    }
+    public PostDetailResponseDTO insert(PostCreateDTO dto)
+        throws Exception {
+
+        // 게시물 저장(아직 헤시태그는 ionsert 되지 않음)
+        Post saved = postRepository.save(dto.toEntity());
+
+        // 헤시태그 저장
+        List<String> hashTags = dto.getHashTags();
+        if(hashTags != null && !hashTags.isEmpty()){
+            hashTags.forEach(ht -> {
+                HashTag savedTag = hashTagRepository.save(
+                        HashTag.builder()
+                                .tagName(ht)
+                                .post(saved)
+                                .build()
+                );
+                /*
+                    Post Entity는 DB에 save를 진행할 때 HashTag에 대한 내용을 갱신하지 않습니다.
+                    HashTag Entity는 따로 save를 진행합니다. (테이블이 각각 나뉘어 있음)
+                    HashTag는 양방향 맵핑이 되어있는 연관관계의 주인이기 때문에 save를 진행할 때 Post를 전달하므로
+                    DB와 Entity와의 상태가 동일하지만,
+                    Post는 HashTag의 정보가 비어있는 상태입니다.
+                    Post Entity에 연관관계 편의 메서드를 작성하여 HashTag의 내용을 동기화 해야
+                    추후에 진행되는 과정에서 문제가 발생하지 않습니다.
+                    (Post를 화면단으로 return -> HashTag들도 같이 가야 함. -> 직접 갱신)
+                    (Post를 다시 SELECT 해서 가져온다??? -> 의미없는 행동.(insert는 트랜잭션 종료 후 진행))
+                 */
+                saved.addHashTag(savedTag);
+            });
+        }
 
 
+
+        return new PostDetailResponseDTO(saved);
     }
 }
